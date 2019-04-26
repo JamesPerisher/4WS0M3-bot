980f984e6b51083af659b301475ea2c52d7c67b5
JKookaburra <JonahKerlin@gmail.com>
2019-04-26 15:39:47 +1000

    Update 1.3.1 Fixed error

--- easyChat.py
+++ easyChat.py
@@ -26,33 +26,35 @@ async def on_message(message):
     global ch
     if message.author == client.user:
         return
-    if message.channel.id == "563532653673054209":
-        if message.author.id == "508296387985801226":
-            mes = message.embeds[0]['description'][3::]
+    if message.channel.id == 563532653673054209:
+        if message.author.id == 508296387985801226:
+            #print(message.embeds[0].description)
+
+            mes = message.Embed(description)
 
             mes = mes.split(">** ")
-
+            print(mes)
             user = mes[0].strip()
             line = "%s> %s" %(user, ">** ".join(mes[1::]))
             print(user, line)
 
-            embed=discord.Embed(title=line, color=0x004672)
+            color=discord.Embed(title=line, color=0x004672)
             try:
                 if mes[1][0] == ">":
-                    embed=discord.Embed(title=line, color=0x009e0a)
+                    color=discord.Embed(title=line, color=0x009e0a)
             except:
                 pass
 
             if user in ["PaulN07","PaulN07\\_1"]:
-                embed=discord.Embed(title=line, color=0x7e0000)
+                color=discord.Embed(title=line, color=0x7e0000)
 
-            if user in ["Jkookaburra"]:
-                embed=discord.Embed(title=line, color=0x1f8b4c)
+            if user in ["JKookaburra", "JKookaburra_1", "JKookaburra_2", "JKookaburra_3", "JKookaburra_4", "JKookaburra_5", "JKookaburra_6"]:
+                color=discord.Embed(title=line, color=0x1f8b4c)
 
             if len(mes) == 1:
-                embed=discord.Embed(title=mes[0], color=0xe56706)
+                color=discord.Embed(title=mes[0], color=0xe56706)
 
             if not line == "":
-                await client.send_message(client.get_channel("565114743975575572"), embed=embed)
+                await (get_channel(565114743975575572)).send(content=color)
 
 client.run(sys.argv[1], bot=False)
